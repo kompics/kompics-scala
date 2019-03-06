105bb6e35c270dcd61d9016fd306d880fad77e54
Lars Kroll <lkroll@kth.se>
2019-03-06 11:53:46 +0100

    added more  variants

--- simulator/kompics-scala-simulator.sbt
+++ simulator/kompics-scala-simulator.sbt
@@ -2,7 +2,7 @@ name := "Kompics-Scala-Simulator"
 
 organization := "se.sics.kompics"
 
-version := "1.0.1"
+version := "1.0.2"
 
 scalaVersion := "2.12.8"
 
--- simulator/src/main/scala/se/sics/kompics/sl/simulator/package.scala
+++ simulator/src/main/scala/se/sics/kompics/sl/simulator/package.scala
@@ -7,39 +7,51 @@ import se.sics.kompics.simulator.adaptor._
 import se.sics.kompics.simulator.adaptor.distributions._
 import se.sics.kompics.simulator.adaptor.distributions.extra._
 
-
-
 package object simulator {
 
-    class IntDistributions(val i: Int) extends AnyVal {
-        def toN(): BasicIntSequentialDistribution = {
-            new BasicIntSequentialDistribution(i)
-        }
+  class IntDistributions(val i: Int) extends AnyVal {
+    def toN(): BasicIntSequentialDistribution = {
+      new BasicIntSequentialDistribution(i)
     }
+  }
 
-    implicit def intToDistribution(i: Int): IntDistributions = new IntDistributions(i);
-    
-    implicit def stochasticProcessToChain(sp: StochasticProcess): StochasticProcessChain = {
-        val spc = new StochasticProcessChain();
-        spc.startWith(sp);
-        spc
-    }
-    
-    implicit def chainToScenario(spc: StochasticProcessChain): SimulationScenario = spc.get()
+  implicit def intToDistribution(i: Int): IntDistributions = new IntDistributions(i);
 
-    
-    def raise[E <: KompicsEvent](count: Int, op: Operation[E]): StochasticProcessBuilder = {
-        val sp = new StochasticProcessBuilder();
-        sp.and(count, op);
-    }
-    
-    def raise[E <: KompicsEvent, N <: Number](count: Int, op: Operation1[E, N], d: Distribution[N]): StochasticProcessBuilder = {
-        val sp = new StochasticProcessBuilder();
-        sp.and(count, op, d);
-    }
-    
-    def raise[E <: KompicsEvent, N <: Number, N2 <: Number](count: Int, op: Operation2[E, N, N2], d: Distribution[N], d2: Distribution[N2]): StochasticProcessBuilder = {
-        val sp = new StochasticProcessBuilder();
-        sp.and(count, op, d, d2);
-    }
+  implicit def stochasticProcessToChain(sp: StochasticProcess): StochasticProcessChain = {
+    val spc = new StochasticProcessChain();
+    spc.startWith(sp);
+    spc
+  }
+
+  implicit def chainToScenario(spc: StochasticProcessChain): SimulationScenario = spc.get()
+
+  def raise[E <: KompicsEvent](count: Int, op: Operation[E]): StochasticProcessBuilder = {
+    val sp = new StochasticProcessBuilder();
+    sp.and(count, op);
+  }
+
+  def raise[E <: KompicsEvent, N <: Number](count: Int, op: Operation1[E, N], d: Distribution[N]): StochasticProcessBuilder = {
+    val sp = new StochasticProcessBuilder();
+    sp.and(count, op, d);
+  }
+
+  def raise[E <: KompicsEvent, N <: Number, N2 <: Number](count: Int, op: Operation2[E, N, N2], d: Distribution[N], d2: Distribution[N2]): StochasticProcessBuilder = {
+    val sp = new StochasticProcessBuilder();
+    sp.and(count, op, d, d2);
+  }
+
+  def raise[E <: KompicsEvent, N <: Number, N2 <: Number, N3 <: Number](count: Int, op: Operation3[E, N, N2, N3], d: Distribution[N], d2: Distribution[N2], d3: Distribution[N3]): StochasticProcessBuilder = {
+    val sp = new StochasticProcessBuilder();
+    sp.and(count, op, d, d2, d3);
+  }
+
+  def raise[E <: KompicsEvent, N <: Number, N2 <: Number, N3 <: Number, N4 <: Number](count: Int, op: Operation4[E, N, N2, N3, N4], d: Distribution[N], d2: Distribution[N2], d3: Distribution[N3], d4: Distribution[N4]): StochasticProcessBuilder = {
+    val sp = new StochasticProcessBuilder();
+    sp.and(count, op, d, d2, d3, d4);
+  }
+
+  def raise[E <: KompicsEvent, N <: Number, N2 <: Number, N3 <: Number, N4 <: Number, N5 <: Number](count: Int, op: Operation5[E, N, N2, N3, N4, N5], d: Distribution[N], d2: Distribution[N2], d3: Distribution[N3], d4: Distribution[N4], d5: Distribution[N5]): StochasticProcessBuilder = {
+    val sp = new StochasticProcessBuilder();
+    sp.and(count, op, d, d2, d3, d4, d5);
+  }
 }
